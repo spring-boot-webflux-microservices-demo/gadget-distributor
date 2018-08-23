88e8be4b2e47e726eb9984124b3a8983aa7286cc
VK <viktoras.lipinskis@gmail.com>
2018-08-23 07:25:29 +0300

    wip

--- src/main/java/com/vk/demo/gadgetdistributor/GadgetDistributorApplication.java
+++ src/main/java/com/vk/demo/gadgetdistributor/GadgetDistributorApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @EnableEurekaClient
+@EnableCircuitBreaker
 @EnableReactiveMongoRepositories
 @AutoConfigureAfter(EmbeddedMongoAutoConfiguration.class)
 @SpringBootApplication

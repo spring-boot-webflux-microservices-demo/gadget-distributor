9958e6f6cbdac4413fec993457da3e35e6363b12
VK <viktoras.lipinskis@gmail.com>
2018-08-23 07:23:24 +0300

    implemented userGadgets saved handler

--- src/main/java/com/vk/demo/gadgetdistributor/handler/GadgetDistributorHandler.java
+++ src/main/java/com/vk/demo/gadgetdistributor/handler/GadgetDistributorHandler.java
@@ -12,7 +12,6 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import javax.validation.constraints.NotNull;
-import java.net.URI;
 
 @Component
 public class GadgetDistributorHandler {
@@ -43,14 +42,19 @@ public class GadgetDistributorHandler {
     public Mono<ServerResponse> saveUserGadget(ServerRequest request) {
         String userId = request.pathVariable("userId");
         String gadgetId = request.pathVariable("gadgetId");
-        Mono<User> user = webClientBuilder.build().get().uri("http://api1/api1/findUser/{id}", userId).retrieve().bodyToMono(User.class);
-        Mono<Gadget> gadget = webClientBuilder.build().get().uri("http://api2/api2/findGadget/{id}", gadgetId).retrieve().bodyToMono(Gadget.class);
 
-        UserGadgets result = new UserGadgets();
+        Mono<User> user = webClientBuilder.build().get()
+                .uri("http://api1/api1/findUser/{id}", userId)
+                .retrieve()
+                .bodyToMono(User.class);
+
+        Mono<Gadget> gadget = webClientBuilder.build().get()
+                        .uri("http://api2/api2/findGadget/{id}", gadgetId)
+                        .retrieve()
+                        .bodyToMono(Gadget.class);
+
         Mono<UserGadgets> userGadgets = Mono.zip(user, gadget, UserGadgets::new);
-        userGadgets.subscribe(u -> {result.setUser(u.getUser()); result.setGadgets(u.getGadgets());});
         Mono<UserGadgets> saved = userGadgets.flatMap(gadgetDistributorRepository::save);
-        return saved.flatMap(s -> ServerResponse.created(URI.create(SAVE_USER_GADGET + userId + SLASH + gadgetId))
-                .body(saved, UserGadgets.class));
+        return saved.flatMap(ug -> ServerResponse.ok().body(Mono.just(ug), UserGadgets.class));
     }
 }
--- src/main/java/com/vk/demo/gadgetdistributor/repositories/GadgetDistributorRepository.java
+++ src/main/java/com/vk/demo/gadgetdistributor/repositories/GadgetDistributorRepository.java
@@ -2,8 +2,10 @@ package com.vk.demo.gadgetdistributor.repositories;
 
 import com.vk.demo.gadgetdistributor.models.UserGadgets;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+@Repository
 public interface GadgetDistributorRepository extends ReactiveCrudRepository<UserGadgets, String> {
     Mono<UserGadgets> findByUserId(String userId);
 }
